cda67d1a37eff094d72043e58416ce63ac8a8690
Olivier Biesmans <olivier.biesmans@blablacar.com>
2017-10-09 10:52:00 +0200

    Add testing for release manifest

--- releases/manifest_test.go
+++ releases/manifest_test.go
@@ -1,1 +1,13 @@
 package releases
+
+import "testing"
+
+func TestReadManifestFromFile(t *testing.T) {
+	man, err := ReadManifestFromFile("./testdata/manifest.yaml")
+	if err != nil {
+		t.Fatalf("Unexpected error : %q", err)
+	}
+	if man.Name != "worker-payment-monitoring" {
+		t.Errorf("Unexpected Name : %q", man.Name)
+	}
+}
