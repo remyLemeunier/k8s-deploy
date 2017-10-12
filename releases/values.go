35ab863741c62a3cea7fe9e88621470817fcfdec
Rémy Lemeunier <remy.lemeunier@blablacar.com>
2017-10-12 15:34:32 +0200

    Fix merge of maps

--- releases/values.go
+++ releases/values.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 
+	"github.com/peterbourgon/mergemap"
 	yaml "gopkg.in/yaml.v2"
 	"k8s.io/helm/pkg/strvals"
 )
@@ -24,7 +25,7 @@ func loadValues(valueFiles []string, values []string) ([]byte, error) {
 		if err := yaml.Unmarshal(bytes, &currentMap); err != nil {
 			return []byte{}, fmt.Errorf("failed to parse %s : %s", filePath, err)
 		}
-		base = mergeValues(base, currentMap)
+		base = mergemap.Merge(base, currentMap)
 	}
 
 	for _, value := range values {
@@ -34,29 +35,4 @@ func loadValues(valueFiles []string, values []string) ([]byte, error) {
 	}
 
 	return yaml.Marshal(base)
-}
-
-func mergeValues(dest map[string]interface{}, src map[string]interface{}) map[string]interface{} {
-	for k, v := range src {
-		if _, exists := dest[k]; !exists {
-			dest[k] = v
-			continue
-		}
-		nextMap, ok := v.(map[string]interface{})
-		if !ok {
-			dest[k] = v
-			continue
-		}
-		if _, exists := dest[k]; !exists {
-			dest[k] = nextMap
-			continue
-		}
-		destMap, isMap := dest[k].(map[string]interface{})
-		if !isMap {
-			dest[k] = v
-			continue
-		}
-		dest[k] = mergeValues(destMap, nextMap)
-	}
-	return dest
 }
--- releases/values_test.go
+++ releases/values_test.go
@@ -9,12 +9,8 @@ func TestLoadValues(t *testing.T) {
 		"./testdata/values1.yaml",
 		"./testdata/values2.yaml",
 	}
-	v, err := loadValues(valueFiles, []string{"a=value", "c=1"})
+	_, err := loadValues(valueFiles, []string{"a=value", "c=1"})
 	if err != nil {
 		t.Errorf("Unexpected err: %q", err)
-	}
-
-	if string(v) != "a: value\nc: 1\ntata: z\ntoto: a\n" {
-		t.Errorf("Unexpected values : %q", v)
 	}
 }
