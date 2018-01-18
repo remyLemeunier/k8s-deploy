18fd40843bc50040899ef8d9fac85377df8ec8c1
Olivier Biesmans <olivier.biesmans@blablacar.com>
2018-01-18 16:00:55 +0100

    Display an error when ther is a tiller enpoint but no pods in it

--- helmclient/endpoint.go
+++ helmclient/endpoint.go
@@ -24,20 +24,12 @@ func GetTillerHosts(clientset *kubernetes.Clientset, namespace string) ([]string
 				tillers = append(tillers, fmt.Sprintf("%s:%d", addr.IP, port.Port))
 			}
 		}
-		//for a := range sub.Addresses {
-		//	epAddrs = append(epAddrs, a)
-		//}
+	}
+	if len(tillers) == 0 {
+		return nil, fmt.Errorf("Endpoint 'tiller' has no ready pods")
 	}
 	return tillers, nil
 }
-
-//func RunTiller(env []string, arg ...string) {
-//	cmd := exec.Command("helm", arg...)
-//	cmd.Env = env
-//	cmd.Stdout = os.Stdout
-//	cmd.Stderr = os.Stderr
-//	cmd.Run()
-//}
 
 func NewKubeClient(configPath string, cluster string) (*kubernetes.Clientset, error) {
 	if configPath == "" {
@@ -60,26 +52,3 @@ func NewKubeClient(configPath string, cluster string) (*kubernetes.Clientset, er
 
 	return kubernetes.NewForConfig(config)
 }
-
-//func main() {
-//	var clusterName string
-//
-//	flag.StringVar(&clusterName, "cluster", "", "The name of the kubeconfig cluster to use")
-//	flag.Parse()
-//
-//	spew.Dump(flag.Args())
-//
-//	cl, err := NewKubeClient("/Users/olivierb/.kube/config", clusterName)
-//	if err != nil {
-//		panic(err.Error())
-//	}
-//
-//	tillers, err := GetTillerHosts(cl, "kube-system")
-//	if err != nil {
-//		panic(err.Error())
-//	}
-//
-//	env := os.Environ()
-//	env = append(env, fmt.Sprintf("HELM_HOST=%s", tillers[0]))
-//	RunTiller(env, flag.Args()...)
-//}
